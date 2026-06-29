c452ea8529436a156b5998af38f87e1d27d86316
Sarah M Moore <smmoor24@asu.edu>
2026-06-29 12:50:36 -0500

    update name

--- morrigan-powers-bs-design-workshop/config.js
+++ morrigan-powers-bs-design-workshop/config.js
@@ -1,7 +1,7 @@
 // Change the information inside the quotes for your new template
 const businessConfig = {
     name: "Morrigan Powers",
-    subtitle: "BS Design & Studio",
+    subtitle: "BS Designs & Workshop",
     owner: "Sarah M. Moore",
     phone: "1-561-517-1278",
     email: "sarahmorrigan.si@gmail.com",
