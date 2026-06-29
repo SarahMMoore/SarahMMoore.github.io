511f042e73d18a1aa1acaf08ca0a9070c2898287
Sarah M Moore <smmoor24@asu.edu>
2026-06-29 09:27:16 -0500

    Updated site add BS Design
    Added storefront for design studio and workshop

---
+++ morrigan-powers-bs-design-workshop/config.js
@@ -0,0 +1,10 @@
+// Change the information inside the quotes for your new template
+const businessConfig = {
+    name: "Morrigan Powers",
+    subtitle: "BS Design & Studio",
+    owner: "Sarah M. Moore",
+    phone: "1-561-517-1278",
+    email: "sarahmorrigan.si@gmail.com",
+    address: "Carlinville, IL",
+    copyright: "2024"
+};
